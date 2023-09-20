799a5c6af6191d8f625fc0e4510ad37b74d350cc
PrinceSaini <sainiprincesaini999@gmail.com>
2023-09-20 14:06:06 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div>
-      
+      {/* <div className="header">
+        Phone Directory
+      </div>
+      <button> Add </button>
+      <div>
+        <span> Name </span><br />
+        <span> Phone </span>
+      </div> */}
+      <label htmlFor="name">Name: </label>
+      <input id="name" type="text" placeholder="Type here" defaultValue="Prince"/>
     </div>
   );
 }
