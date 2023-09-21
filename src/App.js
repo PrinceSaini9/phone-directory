6700e0667a4ceea28d8a70b61a7b0da5ffac194c
PrinceSaini <sainiprincesaini999@gmail.com>
2023-09-21 10:57:10 +0530

    Created Header Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-
+import React, { Component } from "react";
+import Header from "./header.js";
 
 function App() {
   return (
     <div>
-      {/* <div className="header">
-        Phone Directory
-      </div>
+      <Header />
       <button> Add </button>
       <div>
         <span> Name </span><br />
         <span> Phone </span>
-      </div> */}
-      <label htmlFor="name">Name: </label>
-      <input id="name" type="text" placeholder="Type here" defaultValue="Prince"/>
+      </div>
     </div>
   );
 }
