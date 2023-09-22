206e59e29fae65bbb4debe4357838aa10d246508
PrinceSaini <sainiprincesaini999@gmail.com>
2023-09-22 14:35:33 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -3,16 +3,36 @@ import React
 import Header from "./header.js";
 
 function App() {
+  let subs=[
+    {
+      id:1,
+      name:"Prince" ,
+      num: 98769876
+    },
+    {
+      id:2,
+      name:"Shrishti Bhatt" ,
+      num: 987098778
+    }
+  ];
   return (
     <div className="body">
       <Header />
       <button className="bg-success fs-5 m-4 px-4 py-2 text-light text-uppercase border-0 ">
         Add
       </button>
-      <div className="row">
+      <div className="row mb-3">
         <span className="col-4 mx-4 text-uppercase fs-5 fw-bold text-dark text-opacity-50"> Name </span><br />
         <span className="col-4 text-uppercase fs-5 fw-bold text-dark text-opacity-50"> Phone </span>
       </div>
+      {
+        subs.map(sub=>{
+          return <div className="row" key={sub.id}>
+          <span className="col-4 mx-4  fs-5  text-dark"> {sub.name} </span><br />
+          <span className="col-4  fs-5  text-dark "> {sub.num} </span>
+        </div>
+        })
+      }
     </div>
   );
 }
